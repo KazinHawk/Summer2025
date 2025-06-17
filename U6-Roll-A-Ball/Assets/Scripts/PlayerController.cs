5980c618d3a12fd30344b4d0fb362c7927336e60
KazinHawk <Kalani.Harris@hgs.hiddengeniusproject.org>
2025-06-17 12:07:26 -0700

    Moving player
    Giving player movement

---
+++ U6-Roll-A-Ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    private float movementX;
+    private float movementY;
+    [SerializeField] private float speed = 0;
+
+    void Start()
+    {
+        //get & store the Rigidbody component attatched to player
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+
+        rb.AddForce(force:movement * speed);
+
+    }
+    void OnMove(InputValue movementValue)
+    {
+        // convert input value into a Vector2 for movement
+        Vector2 movementVector = movementValue.Get<Vector2>();
+
+    // store the x & y components of the movement 
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+
+        
+    }
+}
