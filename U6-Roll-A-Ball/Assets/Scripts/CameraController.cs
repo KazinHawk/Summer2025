01f4e178ca6c5de9ccb1e5d395b3b53cf66133cc
KazinHawk <Kalani.Harris@hgs.hiddengeniusproject.org>
2025-06-18 13:30:59 -0700

    Inputed Camera
    Worked on moving the camera with the ball

---
+++ U6-Roll-A-Ball/Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public GameObject player;
+    private Vector3 offset;
+
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
